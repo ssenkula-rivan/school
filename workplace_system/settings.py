fc19c494fda9cf2aeba7a91231cb8582f5b4ee06
ssenkula-rivan <cranictech@gmail.com>
2026-04-13 01:40:26 +0300

    feat: add WEB LOGO-01 as favicon across all templates

--- workplace_system/settings.py
+++ workplace_system/settings.py
@@ -214,6 +214,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # WhiteNoise configuration for production
 STORAGES = {
